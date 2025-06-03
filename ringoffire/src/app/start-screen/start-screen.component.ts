1f5c680dc0c9624565e28c44b77bb88252445a4c
iamfrdfa <fri.faraji@gmail.com>
2025-06-03 13:41:55 +0200

    added game component

--- ringoffire/src/app/start-screen/start-screen.component.ts
+++ ringoffire/src/app/start-screen/start-screen.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
-  selector: 'app-start-screen',
-  imports: [],
-  templateUrl: './start-screen.component.html',
-  styleUrl: './start-screen.component.scss'
+    selector: 'app-start-screen',
+    imports: [],
+    templateUrl: './start-screen.component.html',
+    styleUrl: './start-screen.component.scss'
 })
 export class StartScreenComponent {
-
+    constructor(private router: Router) {    }
+    newGame() {
+        //Start Game
+        this.router.navigateByUrl('/game');
+    }
 }
+
