01e76f509322b4ece1857b28081bcfd0b3784952
iamfrdfa <fri.faraji@gmail.com>
2025-06-03 15:11:56 +0200

    added card deck

--- ringoffire/src/app/game/game.component.ts
+++ ringoffire/src/app/game/game.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import {NgForOf, NgStyle} from '@angular/common';
 
 @Component({
   selector: 'app-game',
-  imports: [],
+    imports: [
+        NgForOf,
+        NgStyle
+    ],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
 export class GameComponent {
+    pickCardAnimation: boolean = false;
 
+    takeCard() {
+        this.pickCardAnimation = true;
+    }
 }
