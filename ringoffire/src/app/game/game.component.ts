560cf9b63c4a5e1c6517c2d40d19e1c0b9296bbf
iamfrdfa <fri.faraji@gmail.com>
2025-06-04 23:34:27 +0200

    added player component, player avatar and displaying names

--- ringoffire/src/app/game/game.component.ts
+++ ringoffire/src/app/game/game.component.ts
@@ -1,20 +1,45 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {NgForOf, NgIf, NgStyle} from '@angular/common';
+import {Game} from '../models/game';
+import {PlayerComponent} from '../player/player.component';
 
 @Component({
-  selector: 'app-game',
+    selector: 'app-game',
     imports: [
         NgForOf,
         NgStyle,
-        NgIf
+        NgIf,
+        PlayerComponent
     ],
-  templateUrl: './game.component.html',
-  styleUrl: './game.component.scss'
+    templateUrl: './game.component.html',
+    styleUrl: './game.component.scss'
 })
 export class GameComponent {
     pickCardAnimation: boolean = false;
+    game: Game | undefined;
+    currentCard: string = "";
+
+    ngOnInit(): void {
+        this.newGame();
+    }
+
+    newGame() {
+        this.game = new Game();
+        console.log(this.game);
+    }
 
     takeCard() {
-        this.pickCardAnimation = true;
+        if (!this.pickCardAnimation) {
+            // @ts-ignore
+            this.currentCard = this.game.stack.pop();
+            this.pickCardAnimation = true;
+            console.log('New Card: ' + this.currentCard);
+            console.log('Game is: ', this.game);
+
+            setTimeout(() => {
+                this.game?.playedCards.push(this.currentCard)
+                this.pickCardAnimation = false;
+            }, 1000)
+        }
     }
 }
---
+++ ringoffire/src/app/models/game.ts
@@ -0,0 +1,34 @@
+export class Game {
+    public players:string[] = ['Hans', 'Peter', 'Freddy'];
+    public stack:string[] = [];
+    public playedCards:string[] = [];
+    public currentPlayer:number = 0;
+
+    constructor() {
+        for (let i = 1; i < 14; i++) {
+            this.stack.push('ace_' + i);
+            this.stack.push('hearts_' + i);
+            this.stack.push('clubs_' + i);
+            this.stack.push('diamonds_' + i);
+        }
+
+        shuffle(this.stack);
+    }
+}
+
+function shuffle(array: string | any[]) {
+    let currentIndex = array.length;
+
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+
+        // Pick an remaining element...
+        let randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        // And swap it with the current element.
+        // @ts-ignore
+        [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]];
+    }
+}
